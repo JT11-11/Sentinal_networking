5f404162dcdbe6a935d64226bce7ac56adfc4598
JT11-11 <jaspertsh08@gmail.com>
2025-04-06 23:34:26 +0800

    updated version

--- vERIFY_MY_MAC.cpp
+++ vERIFY_MY_MAC.cpp
@@ -1,3 +1,27 @@
 //
-// Created by jaspe on 4/1/2025.
+// Created by jaspe
 //
+#include <iostream>
+#include <regex>
+#include <string>
+
+bool isValidMAC(const std::string& mac) {
+    std::regex macRegex(R"(^([0-9A-Fa-f]{2}([-:]))([0-9A-Fa-f]{2}\2){4}[0-9A-Fa-f]{2}$)");
+    return std::regex_match(mac, macRegex);
+}
+
+int main() {
+    std::string macAddress;
+    std::cout << "Enter a MAC address: ";
+    std::getline(std::cin, macAddress);
+    macAddress.erase(0, macAddress.find_first_not_of(" \t"));
+    macAddress.erase(macAddress.find_last_not_of(" \t") + 1);
+
+    if (isValidMAC(macAddress)) {
+        std::cout << "Valid MAC address." << std::endl;
+    } else {
+        std::cout << "Invalid MAC address." << std::endl;
+    }
+
+    return 0;
+}
